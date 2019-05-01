2ecc9d1be5b1a3020247801ac3167461cc6a62eb
Tibalt <anton.shheglov.1@gmail.com>
2019-05-02 00:45:24 +0300

    Final commit, it's done

--- storage.py
+++ storage.py
@@ -5,35 +5,44 @@ import tempfile
 import argparse
 import json
 
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')#magic
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.json')#magic
+try:
+    f = open(storage_path, 'r')
+except FileNotFoundError:
+    f = open(storage_path, 'w')
+finally:
+    f.close()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--key")
-parser.add_argument("--value")
+parser.add_argument("--val")
 
 local_storage = {}
 
 args = parser.parse_args().__dict__#added arguments in variable
 
 
-if args['value'] is None:
+if args['val'] is None:
     #Search for value
-    with open(storage_path, 'r+', encoding='utf-8') as f:
+    with open(storage_path, 'r') as f:
         try:
             local_storage = json.load(f)#something wrong
-            print(local_storage.get(args['key'], None))
+            print(local_storage.get(args['key']))
         except json.decoder.JSONDecodeError:
-            print('Error with reading from a file')
+            print('Error 1')
+
 else:
     #Add new value in the file and in the local_storage
-    print(args['value'])
-    with open(storage_path, 'r+', encoding='utf-8') as f:
+    print(args['val'])
+    with open(storage_path, 'r') as f:
         try:
             local_storage = json.load(f)
+            print(local_storage)
         except json.decoder.JSONDecodeError:
-            print('Error with writing in a file')
+            print('Error 2')
         if local_storage.get(args['key']) is None:
-            local_storage[args['key']] = args['value']
+            local_storage[args['key']] = args['val']
         else:
-            local_storage[args['key']] = [local_storage[args['key']], args['value']]
+            local_storage[args['key']] = local_storage[args['key']]+', '+args['val']
+    with open(storage_path, 'w') as f:
         json.dump(local_storage, f)
