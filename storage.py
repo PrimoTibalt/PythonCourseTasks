cac2c736b9d4aa953b66195cea6bde797dfb057c
Tibalt <anton.shheglov.1@gmail.com>
2019-05-01 09:25:32 +0300

    Did the application, but with many errors and it doesn't work

--- storage.py
+++ storage.py
@@ -5,20 +5,35 @@ import tempfile
 import argparse
 import json
 
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')#magic
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--key")
 parser.add_argument("--value")
+
 local_storage = {}
+
 args = parser.parse_args().__dict__#added arguments in variable
-with open(storage_path, 'w') as f:
-    pass
+
+
 if args['value'] is None:
     #Search for value
-    pass
+    with open(storage_path, 'r+', encoding='utf-8') as f:
+        try:
+            local_storage = json.load(f)#something wrong
+            print(local_storage.get(args['key'], None))
+        except json.decoder.JSONDecodeError:
+            print('Error with reading from a file')
 else:
     #Add new value in the file and in the local_storage
-    local_storage[args["key"]] = args["value"]
-    variable = json.dumps(local_storage)
-    print(type(json.loads(variable)), end='')
-    #Ok, it's better to go sleep
+    print(args['value'])
+    with open(storage_path, 'r+', encoding='utf-8') as f:
+        try:
+            local_storage = json.load(f)
+        except json.decoder.JSONDecodeError:
+            print('Error with writing in a file')
+        if local_storage.get(args['key']) is None:
+            local_storage[args['key']] = args['value']
+        else:
+            local_storage[args['key']] = [local_storage[args['key']], args['value']]
+        json.dump(local_storage, f)
