5c37da2eef0e3a364ee8bcf3baff6517f26a478d
Tibalt <anton.shheglov.1@gmail.com>
2019-05-02 01:32:15 +0300

    Second version(witth functools.wraps)

--- DecoratorToJson.py
+++ DecoratorToJson.py
@@ -1,7 +1,10 @@
 #The task is to create decorator for functions, that will make their returned values in json form
 import json
+from functools import wraps
+
 
 def to_json(func):
+    @wraps(func)
     def newFunc(*args, **kargs):
         if func(*args, **kargs) is dict():
             part = json.dump(func(*args, **kargs))
